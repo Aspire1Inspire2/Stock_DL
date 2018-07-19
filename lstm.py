95d8d740e1f4444fe874b800e1fae9b556905fa1
yzhu16 <yzhu16@ncsu.edu>
2018-07-19 17:26:22 -0400

    Finally a running prototype, needs further tunning

--- lstm.py
+++ lstm.py
@@ -14,7 +14,6 @@ import random
 import pickle
 
 torch.manual_seed(1)
-np.random.seed(seed=1)
 random.seed(1)
 
 # Assign the path the the data pickle file
@@ -22,16 +21,15 @@ data_file = open('./data/Normalized_data/normalized_data_ver5.pickle', 'rb')
 data = pickle.load(data_file)
 
 
-def pick_randata(data, length):
+def load_data(data, location, length):
     """
-    Define a function to randomly pick the learning data
+    Load the learning data
     data is the input Pandas DataFrame, 
+    location is the position of the data,
     length is the number of historical entries in the input to the lstm model
     """
-    while True:
-        location = np.random.randint( 1, len(data) - length + 1 )
-        if data.iloc[location].name == data.iloc[location + length].name:
-            return torch.tensor(data.iloc[location : location + length].values)
+    return torch.tensor(data.iloc[location : location + length].values, 
+                        dtype=torch.float)
 
 def build_dataset(data, history_dim, train_size, test_size):
     """
@@ -42,14 +40,42 @@ def build_dataset(data, history_dim, train_size, test_size):
     train_size is the training dataset size
     test_size is the testing dataset size
     """
+    np.random.seed(seed=1)
+    if train_size + test_size > len(data):
+        raise Exception("Training and testing size larger than the total" + 
+                        " number of available data.")
+    else:
+        data_tag = np.arange(len(data) - (history_dim + 1) )
+        np.random.shuffle(data_tag)
+        
+        train_tag = []
+        test_tag = []
+        
+        i = 0
+        for location in data_tag:
+            if train_size < i: break
+            if (data.iloc[location].name == 
+            data.iloc[location + history_dim].name):
+                i = i + 1
+                train_tag.append(location)
+        
+        i = 0
+        for location in np.flip(data_tag, axis = 0):
+            if test_size < i: break
+            if (data.iloc[location].name == 
+            data.iloc[location + (history_dim + 1)].name):
+                i = i + 1
+                test_tag.append(location)
+    return train_tag, test_tag
     
-
 
 # List hyperparameters here
 HISTORY_DIM = 180
-INPUT_DIM = HISTORY_DIM*2
+TRAIN_SIZE = 100
+TEST_SIZE = 20
+INPUT_DIM = 2
 HIDDEN_DIM = 14
-PREDICT_DIM = 1
+PREDICT_DIM = 2
 
 ######################################################################
 # Create the model:
@@ -86,24 +112,26 @@ class LSTM_Predictor(nn.Module):
 ######################################################################
 # Train the model:
 
+train_tag, test_tag = build_dataset(data, HISTORY_DIM, TRAIN_SIZE, TEST_SIZE)
 
 model = LSTM_Predictor(INPUT_DIM, HIDDEN_DIM, PREDICT_DIM)
+#model = model.double()
 
-# Change the loss function to mean square error.
-# This loss function measures distance of prediction to the actuall stock value
+# Use the mean square errorloss function to measures the distance of 
+# prediction from the actuall stock value
 loss_function = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 # See what the scores are before training
 # Note that element i,j of the output is the score for tag j for word i.
 # Here we don't need to train, so the code is wrapped in torch.no_grad()
-with torch.no_grad():
-    inputs = prepare_sequence(training_data[0][0], word_to_ix)
-    tag_scores = model(inputs)
-    print(tag_scores)
+#with torch.no_grad():
+#    inputs = prepare_sequence(training_data[0][0], word_to_ix)
+#    tag_scores = model(inputs)
+#    print(tag_scores)
 
 for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
-    for sentence, tags in training_data:
+    for tag in train_tag:
         # Step 1. Remember that Pytorch accumulates gradients.
         # We need to clear them out before each instance
         model.zero_grad()
@@ -114,27 +142,32 @@ for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is t
 
         # Step 2. Get our inputs ready for the network, that is, turn them into
         # Tensors of word indices.
-        sentence_in = prepare_sequence(sentence, word_to_ix)
-        targets = prepare_sequence(tags, tag_to_ix)
+        input_data = load_data(data, tag, HISTORY_DIM)
+        targets = torch.tensor(
+                data.iloc[tag + 1 : tag + (HISTORY_DIM + 1)].values,
+                                         dtype = torch.float)
 
         # Step 3. Run our forward pass.
-        tag_scores = model(sentence_in)
+        tag_scores = model(input_data)
 
         # Step 4. Compute the loss, gradients, and update the parameters by
         #  calling optimizer.step()
         loss = loss_function(tag_scores, targets)
+        print(loss)
         loss.backward()
         optimizer.step()
 
 # See what the scores are after training
-with torch.no_grad():
-    inputs = prepare_sequence(training_data[0][0], word_to_ix)
-    tag_scores = model(inputs)
-
-    # The sentence is "the dog ate the apple".  i,j corresponds to score for tag j
-    # for word i. The predicted tag is the maximum scoring tag.
-    # Here, we can see the predicted sequence below is 0 1 2 0 1
-    # since 0 is index of the maximum value of row 1,
-    # 1 is the index of maximum value of row 2, etc.
-    # Which is DET NOUN VERB DET NOUN, the correct sequence!
-    print(tag_scores)
+# =============================================================================
+# with torch.no_grad():
+#     inputs = prepare_sequence(training_data[0][0], word_to_ix)
+#     tag_scores = model(inputs)
+# 
+#     # The sentence is "the dog ate the apple".  i,j corresponds to score for tag j
+#     # for word i. The predicted tag is the maximum scoring tag.
+#     # Here, we can see the predicted sequence below is 0 1 2 0 1
+#     # since 0 is index of the maximum value of row 1,
+#     # 1 is the index of maximum value of row 2, etc.
+#     # Which is DET NOUN VERB DET NOUN, the correct sequence!
+#     print(tag_scores)
+# =============================================================================

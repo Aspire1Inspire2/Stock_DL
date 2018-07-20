5c46d15ada7cd67fb823cc4472d63177dfe4e511
yzhu16 <yzhu16@ncsu.edu>
2018-07-19 21:44:11 -0400

    Need to figure out how to use batch size

--- lstm.py
+++ lstm.py
@@ -123,14 +123,21 @@ loss_function = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 # See what the scores are before training
-# Note that element i,j of the output is the score for tag j for word i.
-# Here we don't need to train, so the code is wrapped in torch.no_grad()
-#with torch.no_grad():
-#    inputs = prepare_sequence(training_data[0][0], word_to_ix)
-#    tag_scores = model(inputs)
-#    print(tag_scores)
+# =============================================================================
+with torch.no_grad():
+    for tag in train_tag: #test_tag:
+        input_data = load_data(data, tag, HISTORY_DIM)
+        targets = torch.tensor(
+               data.iloc[tag + 1 : tag + (HISTORY_DIM + 1)].values,
+                                        dtype = torch.float)
+        tag_scores = model(input_data)
+        loss = loss_function(tag_scores, targets)
+        
+        print(loss)
+# =============================================================================
 
-for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
+for epoch in range(300):
+    print(epoch)
     for tag in train_tag:
         # Step 1. Remember that Pytorch accumulates gradients.
         # We need to clear them out before each instance
@@ -153,21 +160,20 @@ for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is t
         # Step 4. Compute the loss, gradients, and update the parameters by
         #  calling optimizer.step()
         loss = loss_function(tag_scores, targets)
-        print(loss)
+        #print(loss)
         loss.backward()
         optimizer.step()
 
 # See what the scores are after training
 # =============================================================================
-# with torch.no_grad():
-#     inputs = prepare_sequence(training_data[0][0], word_to_ix)
-#     tag_scores = model(inputs)
-# 
-#     # The sentence is "the dog ate the apple".  i,j corresponds to score for tag j
-#     # for word i. The predicted tag is the maximum scoring tag.
-#     # Here, we can see the predicted sequence below is 0 1 2 0 1
-#     # since 0 is index of the maximum value of row 1,
-#     # 1 is the index of maximum value of row 2, etc.
-#     # Which is DET NOUN VERB DET NOUN, the correct sequence!
-#     print(tag_scores)
+with torch.no_grad():
+    for tag in train_tag: #test_tag:
+        input_data = load_data(data, tag, HISTORY_DIM)
+        targets = torch.tensor(
+               data.iloc[tag + 1 : tag + (HISTORY_DIM + 1)].values,
+                                        dtype = torch.float)
+        tag_scores = model(input_data)
+        loss = loss_function(tag_scores, targets)
+        
+        print(loss)
 # =============================================================================

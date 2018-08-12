c1b3c5bfb525b515d1d6ae4bd45dd587597c5089
yzhu16 <yzhu16@ncsu.edu>
2018-08-11 22:59:33 -0400

    Change the input data structure

--- DARNN/data_class.py
+++ DARNN/data_class.py
@@ -1,56 +1,82 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug  1 11:12:55 2018
-
-@author: Yuwei Zhu
-"""
-
-import torch
-from torch.utils.data import Dataset
 import pickle
 import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
 
-import numpy.random as random
-random.seed(seed=1)
+#Hyperparameter
+T = 252 # 252 trading days per year
+BATCH_SIZE = 2 # test batch size
 
+
+# Assign the path the the Pandas data file here
+data_file_open = open('normalized_data_6ver.pickle', 'rb')
+
+#Load the original data
+stock_data = pickle.load(data_file_open)
+
+# Set the research begin and end date:
+temp = stock_data.loc['2002-12-31':'2012-12-31']
+
+# From the semi-one-dimension table create a two dim table
+# This two dimension table is used in the Pytorch dataset as input
+temp = temp.reset_index().pivot(index='date', columns='id', values=['prc_diff','vol'])
+temp = temp.swaplevel(axis=1).sort_index(axis=1)
+
+
+# Ignore the following lines, these are some backup lines
+
+# stock_byid = stock_data.swaplevel().sort_index()
+
+# unique_column = np.unique(temp.index.get_level_values(1).values)
+# unique_row = np.unique(temp.index.get_level_values(0).values)
+
+# column_index = pd.MultiIndex.from_product([unique_column,['vol','prc_diff']])
+# row_index = pd.Index(unique_row, name='date')
+
+
+# Define the Dataset class, to be later used in Pytorch Dataloader
 class StockDataset(Dataset):
-    """Face Landmarks dataset."""
+    """
+    Stock dataset.
+    """
 
-    def __init__(self, data_file, batch_dim, device):
+    def __init__(self, pd_data, T, device):
         """
         Args:
-            data_file (string): Path to the data file with stock information.
-            history_dim (int): Number of historical records in each sample.
+            pd_data (pandas DataFrame): The data.
+            T (int): Number of historical records in each learning sample.
+            device: cuda device or cpu.
         """
-        self.batch_dim = batch_dim
+        self.T = T
         self.device = device
         
-        data_file_open = open(data_file, 'rb')
-        self.stock_data = pickle.load(data_file_open)
-        self.stock_data.sort_index()
-        self.stock_index = self.stock_data.index
-        self.stock_index_unique = np.unique(self.stock_index.values)
-        #self.data_tag = random.permutation(self.stock_index_unique)
-        self.stock_index_counts = self.stock_index.value_counts()
-        self.data_tag = self.stock_index_counts.index.values
+        self.stock_data = pd_data
+        self.total_time = len(self.stock_data)
+        self.tot_num = len(np.unique(pd_data.columns.get_level_values(0).values))
                 
     def __len__(self):
-        return len(self.stock_index_unique)
+        return self.total_time - self.T + 1
 
     def __getitem__(self, idx):
-        batch_group = int(idx / self.batch_dim)
-        location = self.data_tag[batch_group * self.batch_dim : 
-            (batch_group + 1) * self.batch_dim]
-        max_length = self.stock_index_counts.loc[location].max() - 1
-        length = self.stock_index_counts.loc[self.data_tag[idx]] - 1
-        pad_length = max_length - length
-        sample = self.stock_data.loc[self.data_tag[idx]].iloc[:-1].values
-        targets = self.stock_data.loc[self.data_tag[idx],
-                                      'pcnt_diff'].iloc[1:].values
-        sample = np.pad(sample, [(0, pad_length), (0, 0)], 
-                                 mode='constant', constant_values=0)
-        targets = np.pad(targets, (0, pad_length), 
-                                 mode='constant', constant_values=0)
+        """
+        Return a tuple, 
+        the first result is the prediction input data
+        the second result is the larning target value
+        
+        The input data is pytorch tensor with the following shape:
+        (number of stocks in the sample, number of timesteps, 2)
+        In the last dimension, there are two numbers, 
+        the first number is price change ('prc_diff')
+        the second number is volumn ('vol')
+        """
+        begin = idx
+        end = idx + self.T
+        
+        sample = self.stock_data.iloc[begin:end].values
+        sample = sample.reshape((self.T,self.tot_num,2)).transpose(1,0,2)
+        
+        targets = self.stock_data.iloc[end].values
+        targets = targets.reshape((self.tot_num,2)).transpose()[0]
         
         return (torch.tensor(np.float64(sample), 
                              dtype = torch.float64,
@@ -59,8 +85,25 @@ class StockDataset(Dataset):
                 torch.tensor(np.float64(targets), 
                              dtype = torch.float64,
                              requires_grad=False,
-                             device = self.device),
-                torch.tensor(np.float64(length),
-                             dtype = torch.float64,
-                             requires_grad=False,
                              device = self.device))
+
+# Here is how to use it:
+# 1. Put the processed two dim table
+# 2. Put the number of trading days in lstm input
+# 3. Put the device name
+stock_dataset = StockDataset(temp, T, 'cpu')
+
+# Let's try out the first sample
+# Compare to temp[10001].iloc[0:253] to see it successfully loaded
+sample, targets = stock_dataset.__getitem__(0)
+print(sample.shape)
+print(targets.shape)
+
+# Assign the Dataloader to automatically load batched data for you
+stock_dataloader = DataLoader(dataset=stock_dataset, batch_size=BATCH_SIZE,
+                              shuffle=False)
+
+# Lets try out the dataloader
+data_iter = stock_dataloader.__iter__()
+data_iter.__init__(stock_dataloader)
+sample, targets = data_iter.__next__()

a34d0f50def10d7487adc4d39cec8b4b7da6b351
yzhu16 <yzhu16@ncsu.edu>
2018-08-14 17:00:19 -0400

    A new data class that would return the exogenous input series as "x", self history as "y" and learning value as "target". The new data class is to be used with "python_stock_data.pickle"
    A new data class that would return the exogenous input series as "x", self history as "y", and target learning value as "target". The new data class is to be used with "python_stock_data.pickle".
    
    Change the tag RET, DATE, PERMNO, VOL as the same of CRSP data base.
    
    Delete the Normalize data file. As the data is directly read from CRSP database.

--- DARNN/data_class.py
+++ DARNN/data_class.py
@@ -1,26 +1,27 @@
 import pickle
 import numpy as np
+import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 
 #Hyperparameter
 T = 252 # 252 trading days per year
 BATCH_SIZE = 2 # test batch size
-
+y_label = 10001
 
 # Assign the path the the Pandas data file here
-data_file_open = open('normalized_data_6ver.pickle', 'rb')
+data_file_open = open('python_stock_data.pickle', 'rb')
 
 #Load the original data
 stock_data = pickle.load(data_file_open)
 
 # Set the research begin and end date:
-temp = stock_data.loc['2002-12-31':'2012-12-31']
+temp = stock_data.loc['2002-12-31':'2012-12-31'].reset_index()
 
 # From the semi-one-dimension table create a two dim table
 # This two dimension table is used in the Pytorch dataset as input
-temp = temp.reset_index().pivot(index='date', columns='id', values=['prc_diff','vol'])
-temp = temp.swaplevel(axis=1).sort_index(axis=1)
+temp = temp.pivot(index='DATE', columns='PERMNO', values=['RET','VOL'])
+temp = temp.swaplevel(axis=1).sort_index(axis=1).fillna(0)
 
 
 # Ignore the following lines, these are some backup lines
@@ -30,7 +31,7 @@ temp = temp.swaplevel(axis=1).sort_index(axis=1)
 # unique_column = np.unique(temp.index.get_level_values(1).values)
 # unique_row = np.unique(temp.index.get_level_values(0).values)
 
-# column_index = pd.MultiIndex.from_product([unique_column,['vol','prc_diff']])
+# column_index = pd.MultiIndex.from_product([unique_column,['VOL','RET']])
 # row_index = pd.Index(unique_row, name='date')
 
 
@@ -40,7 +41,7 @@ class StockDataset(Dataset):
     Stock dataset.
     """
 
-    def __init__(self, pd_data, T, device):
+    def __init__(self, pd_data, T, ylabel, device):
         """
         Args:
             pd_data (pandas DataFrame): The data.
@@ -48,6 +49,7 @@ class StockDataset(Dataset):
             device: cuda device or cpu.
         """
         self.T = T
+        self.ylabel = ylabel
         self.device = device
         
         self.stock_data = pd_data
@@ -66,38 +68,51 @@ class StockDataset(Dataset):
         The input data is pytorch tensor with the following shape:
         (number of stocks in the sample, number of timesteps, 2)
         In the last dimension, there are two numbers, 
-        the first number is price change ('prc_diff')
-        the second number is volumn ('vol')
+        the first number is price change ('RET')
+        the second number is VOLumn ('VOL')
         """
         begin = idx
         end = idx + self.T
         
-        sample = self.stock_data.iloc[begin:end].values
-        sample = sample.reshape((self.T,self.tot_num,2)).transpose(1,0,2)
+        x = self.stock_data.drop(columns=self.ylabel).iloc[begin:end]
+        x = x.swaplevel(axis=1)
+        x['VOL'] = (x['VOL'] - x['VOL'].mean()) / (x['VOL'].max() - x['VOL'].min())
+        x = x.values
+        x = x.reshape((self.T, self.tot_num - 1, 2)).transpose(1, 0, 2)
+        x = torch.tensor(np.float64(x), 
+                          dtype = torch.float64,
+                          requires_grad=False,
+                          device = self.device)
+    
+        y = self.stock_data[self.ylabel].iloc[begin:end]
+        y['VOL'] = (y['VOL'] - y['VOL'].mean()) / (y['VOL'].max() - y['VOL'].min())
+        y = y.values
+        y = torch.tensor(np.float64(y), 
+                         dtype = torch.float64,
+                         requires_grad=False,
+                         device = self.device)
         
-        targets = self.stock_data.iloc[end].values
-        targets = targets.reshape((self.tot_num,2)).transpose()[0]
+        target = self.stock_data[self.ylabel].iloc[end].values[0]
+        #target = targets.reshape((self.tot_num,2)).transpose()[0]
+        target =  torch.tensor(np.float64(target), 
+                               dtype = torch.float64,
+                               requires_grad=False,
+                               device = self.device)
         
-        return (torch.tensor(np.float64(sample), 
-                             dtype = torch.float64,
-                             requires_grad=False,
-                             device = self.device), 
-                torch.tensor(np.float64(targets), 
-                             dtype = torch.float64,
-                             requires_grad=False,
-                             device = self.device))
+        return x, y, target
 
 # Here is how to use it:
 # 1. Put the processed two dim table
 # 2. Put the number of trading days in lstm input
 # 3. Put the device name
-stock_dataset = StockDataset(temp, T, 'cpu')
+stock_dataset = StockDataset(temp, T, y_label, 'cpu')
 
 # Let's try out the first sample
 # Compare to temp[10001].iloc[0:253] to see it successfully loaded
-sample, targets = stock_dataset.__getitem__(0)
-print(sample.shape)
-print(targets.shape)
+x, y, target = stock_dataset.__getitem__(0)
+print(x.size())
+print(y.size())
+print(target.size())
 
 # Assign the Dataloader to automatically load batched data for you
 stock_dataloader = DataLoader(dataset=stock_dataset, batch_size=BATCH_SIZE,
@@ -106,4 +121,4 @@ stock_dataloader = DataLoader(dataset=stock_dataset, batch_size=BATCH_SIZE,
 # Lets try out the dataloader
 data_iter = stock_dataloader.__iter__()
 data_iter.__init__(stock_dataloader)
-sample, targets = data_iter.__next__()
+x_batch, y_batch, target_batch = data_iter.__next__()

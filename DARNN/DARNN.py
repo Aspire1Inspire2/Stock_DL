a1cefd062275202aa5b28b6af6fdbd4faf4b997f
Aspire1Inspire2 <yuweizhu90@gmail.com>
2018-08-17 22:05:38 -0400

    Another try to reduce memory
    The previous push tries to reduce memory usage by replacing the "perceptron" with a linear n-to-1 layer. It also reduces the input size by selecting the stock with the largest trading capital (prc*vol) average per day. These helped a little bit, but not enough.
    
    In this push, the attention weight was assigned as a time independent parameter that does not change at all. Hopefully, it will be optimized between  different epoch. The attention weight is put outside of the time loop. Hopefully, the loop can be rewritten using the lstm instead of the current lstm_cell.

--- DARNN/DARNN.py
+++ DARNN/DARNN.py
@@ -24,7 +24,8 @@ class encoder(nn.Module):
                             dtype=None, device=device, requires_grad=False)
         self.cell = torch.rand([batch_size, hidden_size],
                             dtype=None, device=device, requires_grad=False)
-        
+        self.attn_weights = nn.Parameter(torch.rand([batch_size, n_stock],
+                            dtype=None, device=device, requires_grad=False))
         # perceptron in the paper is taking too much memory
 #        self.perceptron = nn.Sequential(
 #            nn.Linear(in_features = T + 2 * hidden_size, out_features = T),
@@ -44,7 +45,10 @@ class encoder(nn.Module):
         #                               dtype=None, device=self.device, requires_grad=False)
         input_encoded = torch.zeros([self.batch_size, self.T, self.hidden_size],
                                      dtype=None, device=self.device, requires_grad=False)
-        
+        attn_weights = self.attn_weights.transpose(0,1) \
+                                        .repeat(1, 2) \
+                                        .view(self.n_fea, self.batch_size) \
+                                        .transpose(0,1)
         
         for t in range(self.T):
             print(t)
@@ -53,27 +57,24 @@ class encoder(nn.Module):
             # h1, c1 =  self.lstm(input_data[:, t, :], (h0, c0))
             
             # Eqn. 8: concatenate the hidden states with each predictor
-            x = torch.cat((self.hidden.unsqueeze(dim=1) \
-                             .repeat(1, self.n_fea, 1),
-                           self.cell.unsqueeze(dim=1) \
-                             .repeat(1, self.n_fea, 1),
-                           input_data.transpose(1,2)), dim = 2)
-            
-            # Perceptron in the paper is taking too much memory
+            #x = torch.cat((self.hidden.unsqueeze(dim=1) \
+            #                 .repeat(1, self.n_fea, 1),
+            #               self.cell.unsqueeze(dim=1) \
+            #                 .repeat(1, self.n_fea, 1),
+            #               input_data.transpose(1,2)), dim = 2)
+            #
+            # Perceptron in the paper is taking too much memory,
+            # maybe the line below would be helpful
             # e_t = self.perceptron(x).squeeze(dim=2)
-            
-            e_t = self.nto1(x).squeeze(dim=2)
-            e_t = self.attn_linear(e_t.view(self.batch_size, self.n_stock, 2))
-            e_t = e_t.squeeze(dim=2)
+            #
+            #e_t = self.nto1(x).squeeze(dim=2)
+            #e_t = self.attn_linear(e_t.view(self.batch_size, self.n_stock, 2))
+            #e_t = e_t.squeeze(dim=2)
             
             # Eqn. 9: Get attention weights
             # batch_size * n_stock, attn weights with values sum up to n_stock
-            attn_weights = F.softmax(e_t, dim = 1) * self.n_stock 
-            attn_weights = attn_weights.transpose(0,1) \
-                                       .repeat(1, 2) \
-                                       .view(self.n_fea, self.batch_size) \
-                                       .transpose(0,1)
-            print(attn_weights)
+            #attn_weights = F.softmax(e_t, dim = 1) * self.n_stock 
+            
             # Eqn. 10: LSTM
             weighted_input = torch.mul(attn_weights, input_data[:, t, :]) # batch_size * n_stock
             # Fix the warning about non-contiguous memory
